f290f4412054965285c2086fc4b770b77fda7fb1
Aditya <rajawataditya22singh@gmail.com>
2026-09-01 22:27:58 +0530

    initial

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://eventora-backend-3ifv.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
